35313b3d30dcc41478b871e053bdd1efdef3873e
rvlwldev <rvlwlasd77@gmail.com>
2023-01-02 19:39:41 +0900

    feat: 코인목록을 초기화하는 컨트롤러 로직 추가

--- src/main/java/com/mockcryptotrade/Controller/AdminController.java
+++ src/main/java/com/mockcryptotrade/Controller/AdminController.java
@@ -1,4 +1,38 @@
 package com.mockcryptotrade.Controller;
 
+import com.mockcryptotrade.Common.ApiService;
+import com.mockcryptotrade.Domain.Crypto.CryptoInfo;
+import com.mockcryptotrade.Repository.Interface.CryptoInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    ApiService apiService;
+
+    @Autowired
+    CryptoInfoRepo cryptoInfoRepo;
+
+    @PostMapping("/initCryptoList")
+    public String initCrypto(@RequestParam Map<String, Object> param) throws IOException {
+        try {
+            List<CryptoInfo> CryptoInfoList = apiService.initializeCryptoNames();
+            cryptoInfoRepo.saveAll(CryptoInfoList);
+        } catch (Exception e) {
+            return "실패 ㅠㅠ";
+        }
+        return "성공!";
+    }
+
+
 }
