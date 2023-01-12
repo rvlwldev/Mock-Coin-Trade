948bf3141be09abc3342398b9bfd7bdbf0fa2504
rvlwldev <rvlwlasd77@gmail.com>
2023-01-12 21:58:10 +0900

    feat: authenticationManager 및 filterChain 재설정

--- src/main/java/com/mockcryptotrade/Common/Configuration/SecurityConfiguration.java
+++ src/main/java/com/mockcryptotrade/Common/Configuration/SecurityConfiguration.java
@@ -2,8 +2,12 @@ package com.mockcryptotrade.Common.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -12,19 +16,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll()
+        http.authorizeRequests().antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
-                    .loginPage("/login").permitAll()
+                    .loginPage("/auth/login")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/")
                 .and()
                 .csrf().disable();
 
