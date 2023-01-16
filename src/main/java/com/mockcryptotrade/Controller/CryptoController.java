210f686fff41fe688d984c01b0884e8c87e4222b
rvlwldev <rvlwlasd77@gmail.com>
2023-01-16 18:33:16 +0900

    feat: 목록새로고침 post로 변경

--- src/main/java/com/mockcryptotrade/Controller/CryptoController.java
+++ src/main/java/com/mockcryptotrade/Controller/CryptoController.java
@@ -7,7 +7,9 @@ import com.mockcryptotrade.Repository.Crypto.CryptoInitRepo;
 import com.mockcryptotrade.Service.CryptoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -31,7 +33,7 @@ public class CryptoController {
         return view;
     }
 
-    @GetMapping("/refresh")
+    @PostMapping("/refresh")
     public List<CryptoDetail> refreshCryptoDetailList() {
         return getCryptoDetailList();
     }
